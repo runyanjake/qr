c633ebd2e075cec63689549f5cf36fd532a94345
whitney <whitney.server@gmail.com>
2025-08-19 10:58:14 -0700

    Add examples

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import qrcode
 from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers import GappedSquareModuleDrawer
 from qrcode.constants import ERROR_CORRECT_H
 
 data_to_encode = "https://linktr.ee/jakerunyanphotography"
-image_path = "data/photo.jpeg"
+image_path = "input/photo.jpeg"
 
 qr = qrcode.QRCode(
     error_correction=ERROR_CORRECT_H,
     box_size=10,
-    border=4,
+    border=2
 )
 
 qr.add_data(data_to_encode)
@@ -16,10 +17,11 @@ qr.make(fit=True)
 
 img = qr.make_image(
     image_factory=StyledPilImage,
+    module_drawer=GappedSquareModuleDrawer(),
     embeded_image_path=image_path
 )
 
-output_filename = "data/qr_with_logo.png"
+output_filename = "output/output_qr.svg"
 img.save(output_filename)
 
 print(f"Successfully created QR code '{output_filename}' with '{image_path}' embedded.")
