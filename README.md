ad7c4037e10618f28723c91848e5a59268e474c3
Jake Runyan <runyanjake@gmail.com>
2025-08-20 00:54:10 -0700

    Rename script and update README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # QR Codes
 Generating my own QR codes.
+
+## Examples
+```
+python qr-qrcode.py --drawer SquareModuleDrawer --correction ERROR_CORRECT_H --data https://linktr.ee/jakerunyanphotography --input-image ./input/photo.jpeg --output output/output.png
+python qr-qrcode.py --drawer SquareModuleDrawer --correction ERROR_CORRECT_H --data https://linktr.ee/jakerunyanphotography --output output/output.png
+```
--- qr-qrcode.py
+++ qr-qrcode.py
@@ -4,17 +4,15 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import SquareModuleDrawer, GappedSquareModuleDrawer, CircleModuleDrawer, RoundedModuleDrawer, VerticalBarsDrawer, HorizontalBarsDrawer
 from qrcode.constants import ERROR_CORRECT_H, ERROR_CORRECT_Q, ERROR_CORRECT_M, ERROR_CORRECT_L
 
-data_to_encode = "https://linktr.ee/jakerunyanphotography"
-input_image_path = "input/photo.jpeg"
 output_image_path = "output/output.png"
 
 DRAWER_MAP = {
-    "SquareModuleDrawer": moduledrawers.SquareModuleDrawer,
-    "GappedSquareModuleDrawer": moduledrawers.GappedSquareModuleDrawer,
-    "CircleModuleDrawer": moduledrawers.CircleModuleDrawer,
-    "RoundedModuleDrawer": moduledrawers.RoundedModuleDrawer,
-    "VerticalBarsDrawer": moduledrawers.VerticalBarsDrawer,
-    "HorizontalBarsDrawer": moduledrawers.HorizontalBarsDrawer,
+    "SquareModuleDrawer": SquareModuleDrawer,
+    "GappedSquareModuleDrawer": GappedSquareModuleDrawer,
+    "CircleModuleDrawer": CircleModuleDrawer,
+    "RoundedModuleDrawer": RoundedModuleDrawer,
+    "VerticalBarsDrawer": VerticalBarsDrawer,
+    "HorizontalBarsDrawer": HorizontalBarsDrawer,
 }
 
 CORRECTION_MAP = {
@@ -26,11 +24,11 @@ CORRECTION_MAP = {
 
 def main():
     parser = argparse.ArgumentParser(description="Generate a styled QR code with qrcode.")
-    parser.add_argument("--drawer", choices=DRAWER_MAP.keys(), default="square", help="Module drawer style")
-    parser.add_argument("--correction", choices=CORRECTION_MAP.keys(), default="H", help="Error correction level")
+    parser.add_argument("--drawer", choices=DRAWER_MAP.keys(), default="SquareModuleDrawer", help="Module drawer style")
+    parser.add_argument("--correction", choices=CORRECTION_MAP.keys(), default="ERROR_CORRECT_H", help="Error correction level")
     parser.add_argument("--data", required=True, help="Data to encode in the QR code")
-    parser.add_argument("--input-image", required=True, help="Path to the image to embed")
-    parser.add_argument("--output", default="output.png", help="Output file path")
+    parser.add_argument("--input-image", help="Path to the image to embed")
+    parser.add_argument("--output", default="output/output.png", help="Output file path")
 
     args = parser.parse_args()
 
@@ -46,11 +44,19 @@ def main():
     qr.add_data(args.data)
     qr.make(fit=True)
 
-    img = qr.make_image(
-        image_factory=StyledPilImage,
-        module_drawer=drawer_class(),
-        embeded_image_path=args.input_image,
-    )
+    print("args.input_image: " + str(args.input_image))
+
+    if args.input_image is not None:
+        img = qr.make_image(
+            image_factory=StyledPilImage,
+            module_drawer=drawer_class(),
+            embeded_image_path=args.input_image,
+        )
+    else:
+        img = qr.make_image(
+            image_factory=StyledPilImage,
+            module_drawer=drawer_class(),
+        )
 
     img.save(args.output)
 
